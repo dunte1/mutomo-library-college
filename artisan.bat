17644aad65b22170833ab7ab35da90b109685f97
dunte1 <dunte1@users.noreply.github.com>
2026-07-30 16:25:52 +0300

    chore: remove dev artifacts from tracking (docs, mobile, docker, scripts)

--- artisan.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set PHPRC=C:\Users\Lab IX\AppData\Local\Microsoft\WinGet\Packages\PHP.PHP.8.3_Microsoft.Winget.Source_8wekyb3d8bbwe
-set PATH=C:\Users\Lab IX\AppData\Local\Microsoft\WinGet\Packages\PHP.PHP.8.3_Microsoft.Winget.Source_8wekyb3d8bbwe;%PATH%
-php artisan %*
